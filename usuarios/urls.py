e2d8a23fa89bb6bf95c359295bf131175d6c0787
Oteniel <otefran1@gmail.com>
2023-10-04 01:13:05 -0300

    Login e admin

---
+++ usuarios/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views # O ponto faz com que a importação seja dentro desta pasta
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name="cadastro")
+]
